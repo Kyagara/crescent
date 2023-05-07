c027df9620f11c11aeb180d851e3b75b62a8739a
Kyagara <kyagara@proton.me>
2023-05-07 00:44:18 -0300

    more tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,6 @@
-use anyhow::Result;
+use std::{env, fs, os::unix::net::UnixListener, path::PathBuf};
+
+use anyhow::{Context, Result};
 use assert_cmd::Command;
 
 #[test]
@@ -39,6 +41,25 @@ fn log_no_apps_running() -> Result<()> {
 }
 
 #[test]
+fn log_echo() -> Result<()> {
+    let mut cmd = Command::cargo_bin("cres")?;
+
+    cmd.arg("start").arg("/bin/echo").arg("-a").arg("command");
+
+    cmd.assert().success().stdout("Starting daemon\n");
+
+    cmd = Command::cargo_bin("cres")?;
+
+    cmd.arg("log").arg("echo");
+
+    cmd.assert()
+        .success()
+        .stdout("command\n>> Printed 200 lines\n");
+
+    Ok(())
+}
+
+#[test]
 fn send_no_apps_running() -> Result<()> {
     let mut cmd = Command::cargo_bin("cres")?;
 
@@ -47,6 +68,33 @@ fn send_no_apps_running() -> Result<()> {
     cmd.assert()
         .failure()
         .stderr("Error: Application does not exist.\n");
+
+    Ok(())
+}
+
+#[test]
+fn send_command() -> Result<()> {
+    let home = env::var("HOME").context("Error getting HOME env.")?;
+
+    let mut crescent_dir = PathBuf::from(home);
+
+    crescent_dir.push(".crescent");
+    crescent_dir.push("apps");
+    crescent_dir.push("socket_test");
+
+    fs::create_dir_all(&crescent_dir).context("Error creating crescent directory.")?;
+
+    let address = crescent_dir.join("socket_test.sock");
+
+    let _socket = UnixListener::bind(&address)?;
+
+    let mut cmd = Command::cargo_bin("cres")?;
+
+    cmd.arg("send").arg("socket_test").arg("command");
+
+    cmd.assert().success().stdout("Command sent.\n");
+
+    fs::remove_file(address).unwrap();
 
     Ok(())
 }
