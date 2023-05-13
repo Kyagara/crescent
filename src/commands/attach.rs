c05b89546e9fecc6c5a6fc1f8b8f788d9a33327e
Kyagara <kyagara@proton.me>
2023-05-13 18:48:32 -0300

    attach rework, removed ticker

--- src/commands/attach.rs
+++ src/commands/attach.rs
@@ -1,20 +1,17 @@
 use crate::{application, tail};
 use anyhow::{anyhow, Result};
 use clap::Args;
-use crossbeam::{
-    channel::{tick, unbounded, Receiver, Sender},
-    select,
-};
+use crossbeam::channel::{tick, unbounded, Receiver, Sender};
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode, MouseEventKind},
     execute,
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use log::{debug, LevelFilter};
 use std::{
-    io::{self, Stdout, Write},
+    io::{self, Write},
     os::unix::net::UnixStream,
-    path::Path,
+    path::PathBuf,
     thread,
     time::Duration,
 };
@@ -27,7 +24,7 @@ use tui::{
     widgets::{Block, Borders, Paragraph},
     Frame, Terminal,
 };
-use tui_input::{backend::crossterm::EventHandler, Input};
+use tui_input::Input;
 use tui_logger::{init_logger, set_default_level, TuiLoggerWidget, TuiWidgetEvent, TuiWidgetState};
 
 #[derive(Args)]
@@ -37,214 +34,225 @@ pub struct AttachArgs {
     pub name: String,
 }
 
-enum InputMode {
-    Normal,
-    Editing,
-}
-
-struct AttachedTerminal {
-    name: String,
-    logger_state: TuiWidgetState,
+struct AttachTerminal {
+    app_name: String,
+    logger: TuiWidgetState,
     input: Input,
-    input_mode: InputMode,
+    running: bool,
 }
 
-impl AttachedTerminal {
-    fn new(name: String) -> AttachedTerminal {
-        AttachedTerminal {
-            name,
+impl AttachTerminal {
+    fn new(app_name: String) -> AttachTerminal {
+        AttachTerminal {
+            app_name,
             input: Input::default(),
-            input_mode: InputMode::Normal,
-            logger_state: TuiWidgetState::new(),
+            logger: TuiWidgetState::new(),
+            running: true,
         }
     }
+}
+
+enum TerminalEvent {
+    CrosstermEvent(Event),
+    Log(String),
+    Stats(String),
 }
 
 impl AttachArgs {
     pub fn run(self) -> Result<()> {
+        if !application::app_already_running(&self.name)? {
+            return Err(anyhow!("Application not running."));
+        }
+
         init_logger(LevelFilter::Debug).unwrap();
         set_default_level(LevelFilter::Debug);
 
-        let application_socket = application_socket(&self.name)?;
-        let log_events = log_tail_events(&self.name)?;
         let pids = application::app_pids_by_name(&self.name)?;
-        let stats_ticker = stats_update(pids[1])?;
-        let ui_events = ui_events()?;
-        let ticker = tick(Duration::from_millis(20));
+
+        let (sender, receiver) = unbounded();
+        let (socket_sender, socket_receiver): (Sender<String>, Receiver<String>) = unbounded();
+        let (log_sender, log_receiver): (Sender<String>, Receiver<String>) = unbounded();
+
+        let app_dir = application::app_dir_by_name(&self.name)?;
+        let log_dir = app_dir.join(self.name.clone() + ".log");
+        let socket_dir = app_dir.join(self.name.clone() + ".sock");
+
+        event_read_handler(sender.clone());
+        log_handler(log_dir, sender.clone(), log_sender, log_receiver)?;
+        stats_handler(pids[1], sender);
+        socket_handler(socket_dir, socket_receiver)?;
+
+        let mut app = AttachTerminal::new(self.name);
+        let mut stats_list = String::from("Waiting for stats.");
 
         terminal::enable_raw_mode()?;
-
         let mut stdout = io::stdout();
-
         execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-
         let backend = CrosstermBackend::new(stdout);
-
         let mut terminal = Terminal::new(backend)?;
-
-        let mut app = AttachedTerminal::new(self.name);
-
-        let mut stats_list = String::from("Waiting for stats.");
 
         terminal.draw(|f| ui(f, &mut app, &stats_list))?;
 
-        loop {
-            select! {
-                recv(ticker) -> _ => {
+        while app.running {
+            match receiver.recv()? {
+                TerminalEvent::CrosstermEvent(event) => match event {
+                    Event::Key(key) => {
+                        match key.code {
+                            KeyCode::Enter => {
+                                let input = app.input.value().to_string();
+
+                                if input.is_empty() {
+                                    continue;
+                                }
+
+                                app.input.reset();
+                                let message = format!("{}\n", input);
+                                socket_sender.send(message)?;
+                                app.logger.transition(&TuiWidgetEvent::EscapeKey);
+                            }
+                            KeyCode::PageUp => {
+                                app.logger.transition(&TuiWidgetEvent::PrevPageKey);
+                            }
+                            KeyCode::PageDown => {
+                                app.logger.transition(&TuiWidgetEvent::NextPageKey);
+                            }
+                            KeyCode::Esc => break,
+                            _ => {
+                                tui_input::backend::crossterm::EventHandler::handle_event(
+                                    &mut app.input,
+                                    &event,
+                                );
+                            }
+                        };
+
+                        terminal.draw(|f| ui(f, &mut app, &stats_list))?;
+                    }
+                    Event::Mouse(mouse) => {
+                        match mouse.kind {
+                            MouseEventKind::ScrollDown => {
+                                app.logger.transition(&TuiWidgetEvent::NextPageKey);
+                            }
+                            MouseEventKind::ScrollUp => {
+                                app.logger.transition(&TuiWidgetEvent::PrevPageKey);
+                            }
+                            _ => {}
+                        };
+
+                        terminal.draw(|f| ui(f, &mut app, &stats_list))?;
+                    }
+                    Event::Resize(_, _) => {
+                        terminal.draw(|f| ui(f, &mut app, &stats_list))?;
+                    }
+                    _ => {
+                        terminal.draw(|f| ui(f, &mut app, &stats_list))?;
+                    }
+                },
+                TerminalEvent::Log(content) => {
+                    if content.is_empty() {
+                        continue;
+                    }
+
+                    debug!("{content}");
+
                     terminal.draw(|f| ui(f, &mut app, &stats_list))?;
-                },
-                recv(log_events)-> event => {
-                    if let Ok(content) = event {
-                        if content.is_empty() {
-                            continue;
-                        }
-
-                        debug!("{content}");
-                    }
-                },
-                recv(stats_ticker)-> event => {
-                    if let Ok(stats) = event {
-                        stats_list = stats;
-                    }
-                },
-                recv(ui_events) -> event => {
-                    if let Event::Key(key) = event? {
-                        match app.input_mode {
-                            InputMode::Normal => match key.code {
-                                KeyCode::Char('e') => {
-                                    app.input_mode = InputMode::Editing;
-                                }
-                                KeyCode::PageUp | KeyCode::Up => {
-                                    app.logger_state.transition(&TuiWidgetEvent::PrevPageKey);
-                                },
-                                KeyCode::PageDown | KeyCode::Down => {
-                                    app.logger_state.transition(&TuiWidgetEvent::NextPageKey);
-                                },
-                                KeyCode::Char('q') => {
-                                    break
-                                }
-                                _ => {}
-                            },
-                            InputMode::Editing => match key.code {
-                                KeyCode::Enter => {
-                                    let input = app.input.value().to_string();
-
-                                    if input.is_empty() {
-                                        continue;
-                                    }
-
-                                    app.input.reset();
-
-                                    let message = format!("{}\n", input);
-                                    application_socket.send(message)?;
-                                }
-                                KeyCode::PageUp => {
-                                    app.logger_state.transition(&TuiWidgetEvent::PrevPageKey);
-                                },
-                                KeyCode::PageDown => {
-                                    app.logger_state.transition(&TuiWidgetEvent::NextPageKey);
-                                },
-                                KeyCode::Esc => {
-                                    app.input_mode = InputMode::Normal;
-                                }
-                                _ => {
-                                    app.input.handle_event(&Event::Key(key));
-                                }
-                            },
-                        }
-                    }
+                }
+                TerminalEvent::Stats(stats) => {
+                    stats_list = stats;
+                    terminal.draw(|f| ui(f, &mut app, &stats_list))?;
                 }
             }
         }
 
-        close_terminal(&mut terminal)
+        terminal::disable_raw_mode()?;
+
+        execute!(
+            terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        )?;
+
+        terminal.show_cursor()?;
+
+        Ok(())
     }
 }
 
-fn close_terminal(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
-    terminal::disable_raw_mode()?;
-
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-
-    terminal.show_cursor()?;
-
-    Ok(())
+fn event_read_handler(input: Sender<TerminalEvent>) {
+    thread::spawn(move || loop {
+        input
+            .send(TerminalEvent::CrosstermEvent(
+                crossterm::event::read().unwrap(),
+            ))
+            .unwrap()
+    });
 }
 
-fn application_socket(name: &String) -> Result<Sender<String>> {
-    let mut socket_dir = application::app_dir_by_name(name)?;
-
+fn socket_handler(
+    socket_dir: PathBuf,
+    socket_receiver: Receiver<String>,
+) -> Result<(), anyhow::Error> {
     if !socket_dir.exists() {
-        return Err(anyhow!("Application does not exist."));
-    }
-
-    socket_dir.push(name.clone() + ".sock");
-
-    if !Path::new(&socket_dir).exists() {
         return Err(anyhow!("Socket file does not exist."));
     }
-
-    let (sender, receiver): (Sender<String>, Receiver<String>) = unbounded();
 
     let mut stream = UnixStream::connect(socket_dir)?;
 
     thread::spawn(move || {
-        for received in receiver {
+        for received in socket_receiver {
             stream.write_all(received.as_bytes()).unwrap();
             stream.flush().unwrap();
         }
     });
 
-    Ok(sender)
+    Ok(())
 }
 
-fn log_tail_events(name: &String) -> Result<Receiver<String>> {
-    let mut log_dir = application::app_dir_by_name(name)?;
-
-    if !log_dir.exists() {
-        return Err(anyhow!("Application does not exist."));
-    }
-
-    log_dir.push(name.clone() + ".log");
-
+fn log_handler(
+    log_dir: PathBuf,
+    sender: Sender<TerminalEvent>,
+    log_sender: Sender<String>,
+    log_receiver: Receiver<String>,
+) -> Result<(), anyhow::Error> {
     let mut log = tail::Tail::new(log_dir)?;
-
-    let (sender, receiver) = unbounded();
 
     let lines = log.read_lines(200)?;
 
     for line in lines {
-        sender.send(line)?;
+        sender.send(TerminalEvent::Log(line))?;
     }
 
-    thread::spawn(move || log.watch(&sender).unwrap());
+    thread::spawn(move || log.watch(&log_sender).unwrap());
 
-    Ok(receiver)
+    thread::spawn(move || {
+        for line in log_receiver {
+            sender.send(TerminalEvent::Log(line)).unwrap();
+        }
+    });
+
+    Ok(())
 }
 
-fn stats_update(pid: Pid) -> Result<Receiver<String>> {
-    let (sender, receiver) = unbounded();
-
+fn stats_handler(pid: Pid, sender: Sender<TerminalEvent>) {
     let ticker = tick(Duration::from_secs(2));
 
     let mut system = System::new();
-    system.refresh_all();
+    system.refresh_processes();
+    system.refresh_memory();
 
     let cpu_count = system.physical_core_count().unwrap() as f32;
 
     thread::spawn(move || {
         while ticker.recv().is_ok() {
-            system.refresh_all();
+            system.refresh_processes();
+            system.refresh_memory();
 
             let process = match system.process(pid) {
                 Some(process) => process,
                 None => {
                     sender
-                        .send(String::from("Error retrieving process information."))
+                        .send(TerminalEvent::Stats(String::from(
+                            "Error retrieving process information.",
+                        )))
                         .unwrap();
                     break;
                 }
@@ -264,58 +272,27 @@ fn stats_update(pid: Pid) -> Result<Receiver<String>> {
                 load.fifteen,
             );
 
-            sender.send(info).unwrap();
+            sender.send(TerminalEvent::Stats(info)).unwrap();
         }
     });
-
-    Ok(receiver)
 }
 
-fn ui_events() -> Result<Receiver<Event>> {
-    let (sender, receiver) = unbounded();
-
-    thread::spawn(move || loop {
-        sender.send(crossterm::event::read().unwrap()).unwrap()
-    });
-
-    Ok(receiver)
-}
-
-fn ui<B: Backend>(f: &mut Frame<B>, app: &mut AttachedTerminal, stats_list: &String) {
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut AttachTerminal, stats_list: &String) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Min(1), Constraint::Max(3), Constraint::Max(3)].as_ref())
         .split(f.size());
 
-    let help_message = match app.input_mode {
-        InputMode::Normal => vec![
-            Span::raw("Press "),
-            Span::styled(
-                "q",
-                Style::default()
-                    .add_modifier(Modifier::BOLD)
-                    .add_modifier(Modifier::ITALIC),
-            ),
-            Span::raw(" to exit, "),
-            Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-            Span::raw(" to start editing."),
-        ],
-        InputMode::Editing => vec![
-            Span::raw("Press "),
-            Span::styled(
-                "Esc",
-                Style::default()
-                    .add_modifier(Modifier::BOLD)
-                    .add_modifier(Modifier::ITALIC),
-            ),
-            Span::raw(" to stop editing."),
-        ],
-    };
+    let text_style = Style::default()
+        .add_modifier(Modifier::BOLD)
+        .fg(Color::LightCyan);
+
+    let app_name = Span::styled(app.app_name.as_str(), text_style);
 
     let tui_logger = TuiLoggerWidget::default()
         .block(
             Block::default()
-                .title(app.name.as_str())
+                .title(app_name)
                 .title_alignment(Alignment::Left)
                 .borders(Borders::ALL),
         )
@@ -324,29 +301,27 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut AttachedTerminal, stats_list: &Str
         .output_target(false)
         .output_file(false)
         .output_line(false)
-        .state(&app.logger_state);
+        .state(&app.logger);
 
     f.render_widget(tui_logger, chunks[0]);
 
+    let stats_text = Span::styled("Stats", text_style);
+
     let stats = Paragraph::new(stats_list.to_owned())
-        .block(Block::default().borders(Borders::ALL).title("Stats"));
+        .block(Block::default().borders(Borders::ALL).title(stats_text));
 
     f.render_widget(stats, chunks[1]);
 
+    let input_text = Span::styled("Input", text_style);
+
     let tui_input = Paragraph::new(app.input.value())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
-        .block(Block::default().borders(Borders::ALL).title(help_message));
+        .style(Style::default())
+        .block(Block::default().borders(Borders::ALL).title(input_text));
 
     f.render_widget(tui_input, chunks[2]);
 
-    match app.input_mode {
-        InputMode::Normal => {}
-        InputMode::Editing => f.set_cursor(
-            chunks[2].x + app.input.visual_cursor() as u16 + 1,
-            chunks[2].y + 1,
-        ),
-    }
+    f.set_cursor(
+        chunks[2].x + app.input.visual_cursor() as u16 + 1,
+        chunks[2].y + 1,
+    )
 }
